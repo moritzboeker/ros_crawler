76ab4b75f4b1d6ec47d04ce4c35dbb9d92e3c7c9
Moritz Emanuel Böker <moritz.boeker@gmx.de>
2026-02-28 16:54:03 +0100

    feat(ros2_sparkfun_otos_driver): Optical tracking odometry sensor (OTOS) publishing odom

--- bringup/launch/bringup.py
+++ bringup/launch/bringup.py
@@ -8,46 +8,61 @@ from launch.actions import DeclareLaunchArgument
 from launch.actions import LogInfo
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
-pwm_pca9685_config = PathJoinSubstitution([FindPackageShare('bringup'), 'config', 'pwm_pca9685_ackermann_drive.yaml'])
+ydlidar_x2_cfg = PathJoinSubstitution([FindPackageShare('bringup'), 'config', 'ydlidar_x2_cfg.yaml'])
+pwm_pca9685_cfg = PathJoinSubstitution([FindPackageShare('bringup'), 'config', 'pwm_pca9685_cfg.yaml'])
+sparkfun_otos_cfg = PathJoinSubstitution([FindPackageShare('bringup'), 'config', 'sparkfun_otos_cfg.yaml'])
 
 def generate_launch_description():
-    driver_node = LifecycleNode(package='ydlidar_ros2_driver',
-                                executable='ydlidar_ros2_driver_node',
-                                name='ydlidar_ros2_driver_node',
-                                output='screen',
-                                emulate_tty=True,
-                                parameters=[PathJoinSubstitution([FindPackageShare('bringup'), 'config', 'ydlidar_x2.yaml'])],
-                                namespace='/',
-                                )
-    tf2_node = Node(package='tf2_ros',
-                    executable='static_transform_publisher',
-                    name='static_tf_pub_laser',
-                    arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','laser_frame'],
-                    )
+    ydlidar_x2_node = LifecycleNode(
+                package='ydlidar_ros2_driver',
+                executable='ydlidar_ros2_driver_node',
+                name='ydlidar_ros2_driver_node',
+                output='screen',
+                emulate_tty=True,
+                parameters=[ydlidar_x2_cfg],
+                namespace='/',
+                )
+    ydliard_tf2_node = Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                name='static_tf_pub_laser',
+                arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','laser_frame'],
+                )
 
-    pwm_node = Node(package='pwm_pca9685', 
-                    executable='pca9685_node',
-                    name='pwm_node',
-                    remappings=[
-                        ('/command', '/command'),
-                    ],
-                    parameters=[pwm_pca9685_config],
-                    )
+    pwm_pca9685_node = Node(
+                package='pwm_pca9685', 
+                executable='pca9685_node',
+                name='pwm_node',
+                remappings=[
+                ('/command', '/command'),
+                ],
+                parameters=[pwm_pca9685_cfg],
+                )
 
-    ackermann_to_pwm_node = Node(package='pwm_pca9685', 
-                    executable='ackermann_to_pwm_node',
-                    name='ackermann_to_pwm_node',
-                    remappings=[
-                        ('/ackermann_cmd', '/ackermann_cmd'),
-                        ('/command', '/command'),
-                    ],
-                    parameters=[pwm_pca9685_config],
-                    )
+    ackermann_to_pwm_node = Node(
+                package='pwm_pca9685', 
+                executable='ackermann_to_pwm_node',
+                name='ackermann_to_pwm_node',
+                remappings=[
+                ('/ackermann_cmd', '/ackermann_cmd'),
+                ('/command', '/command'),
+                ],
+                parameters=[pwm_pca9685_cfg],
+                )
+
+    sparkfun_otos_node = Node(
+                package='sparkfun_otos_driver',
+                executable='otos_node',
+                name='otos',
+                output='screen',
+                parameters=[sparkfun_otos_cfg],
+                )
 
     return LaunchDescription([
-        driver_node,
-        tf2_node,
-        pwm_node,
+        ydlidar_x2_node,
+        ydliard_tf2_node,
+        pwm_pca9685_node,
         ackermann_to_pwm_node,
+        sparkfun_otos_node,
     ])
 
